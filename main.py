b79b0555a942d37315f9acc74eda24abe8e3c7cc
walei <>
2023-02-26 22:27:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+
+import random
+random_number=random.randint(1,2)
+if random_number==1:
+  print("heads")
+else:print("tails")
+  
+
+
+
+
+
+
+
+
